21a12de8f74f8819ecd8976fd6c9f8ff08387a16
crazyapril <crazyaprilzh@gmail.com>
2018-06-04 23:09:32 +0800

    add typhoon page

---
+++ src/pages/typhoon/page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class TyphoonPage extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      activeStorms: {
+        time: '',
+        info: []
+      },
+      dayRange: 10
+    }
+  }
+
+}
+
---
+++ src/pages/typhoon/view.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const view = () => {
+    return (
+        <h5>Page</h5>
+    );
+}
+
+export default view;
